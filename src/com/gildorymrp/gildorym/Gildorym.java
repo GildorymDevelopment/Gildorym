989ad765d066041f2ac32688f6d38582354b258c
Shayne <melodicbusiness@yahoo.com>
2013-11-18 06:43:20 -0500

    Fall damage cleanup

--- src/com/gildorymrp/gildorym/Gildorym.java
+++ src/com/gildorymrp/gildorym/Gildorym.java
@@ -42,95 +42,112 @@ public class Gildorym extends JavaPlugin {
 		this.getServer().getPluginManager().registerEvents(new SignChangeListener(), this);
 	}
 
-	public void onInjury(Player player, String type, int dieSize, int fallDistance, int roll) {
-		int severity, fallInFeet, rollPercent, severityPercent;
+	public void onInjury(Player injured, String type, int dieSize, int fallDistance, int roll) {
+		ChatColor b = ChatColor.BLUE;
+		ChatColor r = ChatColor.RED;
+		ChatColor w = ChatColor.WHITE;
+		ChatColor dr = ChatColor.DARK_RED;
+		ChatColor g = ChatColor.GOLD;
+		
+		int severity, fallInFeet, rollPercent, severityPercent, x, y, z;
 		
 		fallInFeet = fallDistance * 3;
 		severity = (new Random()).nextInt(dieSize) + 1;
 		rollPercent = roll * 5;
 		severityPercent = severity * 2;
+		x = injured.getLocation().getBlockX();
+		y = injured.getLocation().getBlockY();
+		z = injured.getLocation().getBlockZ();
 		
-		String messageP1, messageP2, reflex, injury, alert1, alert2; 
+		String message, reflexAlert, injuryAlert, alert, injury, deathLocation;
 
-		messageP1 = "You have fallen ";
-		messageP2 = ChatColor.WHITE + "" + fallInFeet + "";
-		reflex = ChatColor.RED + "Reflex: " + rollPercent + "%";
-		injury = ChatColor.RED + "  Injury: " + severityPercent + "%";
-		alert1 = player.getName() + ChatColor.BLUE + " has just fallen " + ChatColor.WHITE + fallDistance + ChatColor.BLUE + " blocks, taking a " + ChatColor.DARK_RED + " major " + ChatColor.BLUE + "injury. Check on them if you want.";
-		alert2 = player.getName() + ChatColor.BLUE + " has just fallen " + ChatColor.WHITE + fallDistance + ChatColor.BLUE + " blocks, taking a " + ChatColor.GOLD + " minor " + ChatColor.BLUE + "injury. Check on them if you want.";
+		message = "You have fallen " + w + "" + fallInFeet + "";
+		reflexAlert = r + "Reflex: " + w + rollPercent + r + "%";
+		injuryAlert = r + "  Injury: " + w + severityPercent + r + "%";
+		deathLocation = injured.getWorld().getName() + ": x: " + x + " y: " + y + " z: " + z;
+		alert = injured.getName() + b + " has just fallen " + w + fallDistance + b + " blocks, recieving ";
 		
 		if(type.equalsIgnoreCase("none")){
-			player.sendMessage(reflex);
-			player.sendMessage(ChatColor.BLUE + messageP1 + messageP2 + ChatColor.BLUE + " feet, escaping without injury.");
+			message += b + "feet";
+			injury = " escaping without injury.";
+			injured.sendMessage(reflexAlert);
+			injured.sendMessage(b+ message + b + injury);
 			
 		} else if (type.equalsIgnoreCase("major")) {
-			for (Player player2 : Bukkit.getServer().getOnlinePlayers()) {
-				if (player2.hasPermission("gildorym.falldamage.alert")) {
-					player2.sendMessage(alert1);
-				}
-			}
+			message += r + " feet, recieving ";
+			injury = dr + "error";
 			
 			if (severity < 16) {
-				player.sendMessage(reflex + injury);
-				player.sendMessage(ChatColor.RED + messageP1 + messageP2 + " feet, receiving a Punctured Organ (Anything but the Heart), Completely Crushed Limb, Cracked Skull or Cracked Vertebra; 4 Damage.");
-				player.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 35000, 4), true); 
-				player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 35000, 4), true);
+				injury = "a Punctured Organ (Anything but the Heart), Completely Crushed Limb, Cracked Skull or Cracked Vertebra; 4 Damage.";
+				injured.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 35000, 3), true); 
+				injured.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 35000, 3), true);
 				
 			} else if (severity < 31) {
-				player.sendMessage(reflex + injury);
-				player.sendMessage(ChatColor.RED + messageP1 + messageP2 + " feet, receiving a Crushed Small limb (Such as hand/foot), Shattered Bones, or Severe Concussion; 3 Damage.");
-				player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 35000, 4), true);
+				injury = "a Crushed Small limb (Such as hand/foot), Shattered Bones, or Severe Concussion; 3 Damage.";
+				injured.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 35000, 3), true);
 				
 			} else if (severity < 51) {
-				player.sendMessage(reflex + injury);
-				player.sendMessage(ChatColor.RED + messageP1 + messageP2 + " feet, receiving a Cleanly Broken Bone, Torn Major Muscle, or Loss of a Minor Limb; 2 Damage.");
-				player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 35000, 3), true);
+				injury = "a Cleanly Broken Bone, Torn Major Muscle, or Loss of a Minor Limb; 2 Damage.";
+				injured.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 35000, 2), true);
 				
+			}
+			injured.sendMessage(reflexAlert + injuryAlert);
+			injured.sendMessage(r + message + r + injury);
+			
+			for (Player player : Bukkit.getServer().getOnlinePlayers()) {
+				if (player.hasPermission("gildorym.falldamage.alert")) {
+					player.sendMessage(alert + r + injury);
+					player.sendMessage(reflexAlert + r + "  Injury: " + w + severity);
+				}
 			}
 		} else if (type.equalsIgnoreCase("minor")) {
-			for (Player player2 : Bukkit.getServer()
-					.getOnlinePlayers()) {
-				if (player2.hasPermission("gildorym.falldamage.alert")) {
-					player2.sendMessage(alert2);
-				}
-			}
+			message += g + " feet, recieving ";
+			injury = dr + "error";
 			
 			if (severity < 11) {
-				player.sendMessage(reflex + injury);
-				player.sendMessage(ChatColor.GOLD + messageP1 + messageP2 + " feet, receiving a Minor Fracture, Brused Ribs, or Dislocation; 1 damage.");
-				player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 35000, 2), true);
+				injury = "a Minor Fracture, Brused Ribs, or Dislocation; 1 damage.";
+				injured.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 35000, 1), true);
 				
 			} else if (severity < 21) {
-				player.sendMessage(reflex + injury);
-				player.sendMessage(ChatColor.GOLD + messageP1 + messageP2 + " feet, receiving a Minor Torn Ligement, Chipped Bone, or Light Concussion; 1 damage.");
-				player.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 35000, 2), true);
+				injury = "a Minor Torn Ligement, Chipped Bone, or Light Concussion; 1 damage.";
+				injured.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 35000, 1), true);
 				
 			} else if (severity < 35) {
-				player.sendMessage(reflex + injury);
-				player.sendMessage(ChatColor.GOLD + messageP1 + messageP2 + " feet, receiving a Mild Sprain, Mild Laceration, or Badly Pulled Muscle; 1 damage.");
-				player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 15000, 2), true);
+				injury = "a Mild Sprain, Mild Laceration, or Badly Pulled Muscle; 1 damage.";
+				injured.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 15000, 1), true);
 				
 			} else if (severity < 50) {
-				player.sendMessage(reflex + injury);
-				player.sendMessage(ChatColor.GOLD + messageP1 + messageP2 + " feet, receiving Bruises, Mild Lacerations, or Minor Sprain; 1 damage.");
-				player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 5000, 1), true);
+				injury = "Bruises, Mild Lacerations, or Minor Sprain; 1 damage.";
+				injured.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 5000, 0), true);
 			} else if (severity == 50) {
-				player.sendMessage(reflex + injury);
-				player.sendMessage(ChatColor.GOLD + messageP1 + messageP2 + " feet, receiving only a few scratches.");
+				injury = "only a few scratches.";
 				
 			}
-		} else if(type.equalsIgnoreCase("death")){
-			for (Player player2 : Bukkit.getServer()
-					.getOnlinePlayers()) {
-				if (player2.hasPermission("gildorym.falldamage.alert")) {
-					player2.sendMessage(player.getName() + ChatColor.BLUE + " has just fallen " + fallDistance + " blocks, and " + ChatColor.DARK_RED + "died.");
+			injured.sendMessage(reflexAlert + injuryAlert);
+			injured.sendMessage(g + message + g + injury);
+			
+			for (Player player : Bukkit.getServer().getOnlinePlayers()) {
+				if (player.hasPermission("gildorym.falldamage.alert")) {
+					player.sendMessage(alert + g + injury);
+					player.sendMessage(reflexAlert + r + "  Injury: " + w + severity);
 				}
 			}
-			player.sendMessage(ChatColor.DARK_RED + messageP1 + messageP2 + " feet, and died.");
-			player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 999999999, 10), true);
-			Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "warp deathbox " + player.getName());
+		} else if(type.equalsIgnoreCase("death")){
+			message += dr + " feet, ";
+			injury = "and died";
+			injured.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 999999999, 10), true);
+			
+			injured.sendMessage(dr + message + dr + injury);
+			for (Player player : Bukkit.getServer().getOnlinePlayers()) {
+				if (player.hasPermission("gildorym.falldamage.alert")) {
+					player.sendMessage(alert + dr + injury);
+					player.sendMessage(reflexAlert);
+					player.sendMessage(deathLocation);
+				}
+			}
+			Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "warp deathbox " + injured.getName());
 		} else {
-			player.sendMessage(ChatColor.RED + "Error : Injury type "
+			injured.sendMessage(ChatColor.RED + "Error : Injury type "
 					+ ChatColor.RESET + type + ChatColor.RED
 					+ " does not exist.");
 		}
