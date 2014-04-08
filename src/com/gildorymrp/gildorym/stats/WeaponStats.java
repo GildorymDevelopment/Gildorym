2bcf025c6e49e35b30172e71446ca8238ad85ed6
Hohahihehu <oaieuy@hotmail.com>
2014-04-07 21:28:37 -0400

    Update WeaponStats.java

--- src/com/gildorymrp/gildorym/stats/WeaponStats.java
+++ src/com/gildorymrp/gildorym/stats/WeaponStats.java
@@ -7,41 +7,34 @@ public class WeaponStats {
 	public int getMeleeAttack(ItemStack weapon) {
 		if (weapon != null)
 			switch (weapon.getType()) {
-			case STONE_SPADE:
-			case WOOD_PICKAXE:
-			case STONE_PICKAXE:
 			case IRON_PICKAXE:
 			case IRON_HOE:
-				return 1;
+			case STONE_SPADE:
+			case STONE_PICKAXE:
+			case WOOD_HOE:
+			case WOOD_PICKAXE:
 			case WOOD_SWORD:
 				return 1;
+			case DIAMOND_PICKAXE:
+			case GOLD_SWORD:
+			case STONE_AXE:
+			case STONE_HOE:
 			case STONE_SWORD:
 			case WOOD_AXE:
-			case DIAMOND_PICKAXE:
 				return 2;
-			case STONE_AXE:
-				return 2;
+			case DIAMOND_HOE:
+			case GOLD_AXE:
+			case GOLD_HOE:
+			case GOLD_SPADE:
 			case IRON_SWORD:
 			case IRON_SPADE:
 			case IRON_AXE:
-			case DIAMOND_HOE:
 				return 3;
 			case DIAMOND_SPADE:
 				return 4;
 			case DIAMOND_SWORD:
 			case DIAMOND_AXE:
 				return 5;
-			case WOOD_HOE:
-				return 1;
-			case STONE_HOE:
-				return 2;
-			case GOLD_SWORD:
-				return 2;
-			case GOLD_SPADE:
-				return 3;
-			case GOLD_AXE:
-			case GOLD_HOE:
-				return 3;
 			default:
 				return 0;
 		}
@@ -64,7 +57,7 @@ public class WeaponStats {
 			case GOLD_PICKAXE:
 				return 4;
 			default:
-					return 0;
+				return 0;
 			}
 		return 0;
 
@@ -73,22 +66,17 @@ public class WeaponStats {
 	public int getMagicAttack(ItemStack weapon) {
 		if (weapon != null)
 			switch (weapon.getType()) {
-			case WOOD_HOE:
-				return 3;
+			case ENDER_PEARL:
+			case GOLD_SPADE:
 			case STONE_HOE:
 				return 2;
-			case GOLD_SWORD:
-				return 3;
-			case GOLD_SPADE:
-				return 2;
-			case GOLD_AXE:
-			case GOLD_HOE:
-				return 4;
-			case ENDER_PEARL:
-				return 2;
 			case EYE_OF_ENDER:
+			case GOLD_SWORD:
+			case WOOD_HOE:
 				return 3;
 			case BLAZE_ROD:
+			case GOLD_AXE:
+			case GOLD_HOE:
 			case GOLDEN_APPLE:
 				return 4;
 			case WOOD_SPADE:
